c369488ab8e7c8293bb30d83373dd96a11e3b1f9
Fernandex360 <roy360erick@gmail.com>
2018-06-27 20:00:58 -0500

    Eliminacion de img de cache

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,8 +1,4 @@
 @extends('layouts.app')
 @section('content')
-<<<<<<< HEAD
-
-=======
->>>>>>> 7348d25aa9d8fc588a16461d3e83c20fd68bf96e
  <div style="height:80vh">{!! Mapper::render()!!}</div>
 @endsection
