c9d2e1ff204efedc64653971f5d93641702ec77f
Fernandex360 <roy360erick@gmail.com>
2018-06-29 17:55:24 -0500

    Nosotros Actulizado

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -1,124 +1,14 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>@yield('app.name', 'Denuncias Anónimas')</title>
-
-    <!-- Scripts -->
-    
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
-
-
-
-	
-	<!-- Animate.css -->
+@extends('layouts.app')
+@section('content')
+	<link rel="dns-prefetch" href="https://fonts.gstatic.com">
 	<link rel="stylesheet" href="../shaape/css/animate.css">
-	<!-- Icomoon Icon Fonts-->
+
 	<link rel="stylesheet" href="../shaape/css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="../shaape/css/bootstrap.css">
-
-	<!-- Magnific Popup -->
 	<link rel="stylesheet" href="../shaape/css/magnific-popup.css">
-
-	<!-- Theme style  -->
-
-	<!-- Modernizr JS -->
 	<script src="../shaape/js/modernizr-2.6.2.min.js"></script>
-
-{!! Html::style('assets/css/material-bootstrap-wizard.css') !!}
-    {!! Html::style('bootstrap4/css/bootstrap.min.css') !!}
-			<link rel="stylesheet" href="../shaape/css/style.css">
+	<link rel="stylesheet" href="../shaape/css/style.css">
 	
-
-	</head>
-	<body>
-		
-	<div class="fh5co-loader"></div>
-	<nav  class="navbar-expand-lg navbar navbar-light " style="background-color: rgb(0, 194, 169);">
-      <a class="navbar-brand ml-5" href="/">Menu</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse mr-3" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            @guest
-            <li class="nav-item">
-                <a class="nav-link" href="principal">INICIO</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="aplicacion">APLICACIÓN</a>
-            </li>
-             <li class="nav-item">
-                <a class="nav-link" href="nosotros">NOSOTROS</a>
-            </li>
-            @else
-            <li class="nav-item">
-                <a class="nav-link" href="principal">INICIO</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="denunciar">DENUNCIAR</a>
-            </li>
-             <li class="nav-item">
-                <a class="nav-link" href="estadistica">ESTADISTICA</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="aplicacion">APLICACIÓN</a>
-            </li>
-             <li class="nav-item">
-                <a class="nav-link" href="nosotros">NOSOTROS</a>
-            </li>
-            @endguest
-        </ul>
-        <ul class="navbar-nav ml-5">
-                        <!-- Authentication Links -->
-            @guest
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-            </li>
-            @else
-            <li class="nav-item dropdown ">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ Auth::user()->name }}
-                <img class="rounded-circle ml-2 mr-2" height="40px" src="{{ Auth::user()->avatar }}">
-                </a>
-            <div class="dropdown-menu dropdown-menu-right  ml-2"  aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Salir') }}
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </div>
-            </li>
-            @endguest
-        </ul>
-      </div>
-    </nav>
-	<div id="page">
-
-	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(../shaape/images/lima.jpg);">
+	<header id="fh5co-header" class="fh5co-cover h-25" role="banner" style="background-image:url(../shaape/images/lima.jpg);">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row">
@@ -242,7 +132,4 @@
 	<script src="../shaape/js/magnific-popup-options.js"></script>
 	<!-- Main -->
 	<script src="../shaape/js/main.js"></script>
-
-	</body>
-</html>
-
+@endsection
