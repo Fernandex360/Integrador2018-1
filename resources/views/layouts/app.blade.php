03b9e2124afb84f517fd64e9735bfcb54db0c417
Jose <jose.cristobal@tecsup.edu.pe>
2018-06-30 20:36:37 -0500

    Denuncia Actualizada

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,16 @@
     
      <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
 
-    {!! Html::style('assets/css/material-bootstrap-wizard.css') !!}
-    {!! Html::style('bootstrap4/css/bootstrap.min.css') !!}
+    {!! Html::style('../assets/css/material-bootstrap-wizard.css') !!}
+    {!! Html::style('../bootstrap4/css/bootstrap.min.css') !!}
     
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
 
-    {!! Html::script('assets/js/jquery-2.2.4.min.js') !!}
-    {!! Html::script('assets/js/bootstrap.min.js') !!}
-    {!! Html::script('assets/js/jquery.bootstrap.js') !!}
-    {!! Html::script('assets/js/material-bootstrap-wizard.js') !!}
-    {!! Html::script('assets/js/jquery.validate.min.js') !!}
+    {!! Html::script('../assets/js/jquery-2.2.4.min.js') !!}
+    {!! Html::script('../assets/js/bootstrap.min.js') !!}
+    {!! Html::script('../assets/js/jquery.bootstrap.js') !!}
+    {!! Html::script('../assets/js/material-bootstrap-wizard.js') !!}
+    {!! Html::script('../assets/js/jquery.validate.min.js') !!}
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
 </head>
@@ -96,7 +97,8 @@
     </nav>
         <main class="col-md-12 m-0 p-0">
             @yield('content')
-            
+     
+
         </main>
     </div>
     
